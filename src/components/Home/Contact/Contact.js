4b94c8f64b01abc0b928ca7cc8840850626d6a65
Lucas Melo <luucasmell@gmail.com>
2020-04-16 20:33:00 -0300

    Create contact component

---
+++ src/components/Home/Contact/Contact.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Contact.scss";
+import { useWindowSizeHook } from "../../../hooks/windowHooks";
+
+const Contact = () => {
+  const { height } = useWindowSizeHook();
+  return (
+    <section className="Contact" style={{ minHeight: height }}>
+      <h3>Entre em contato</h3>
+      <p>Tem alguma dúvida ou problema? Fale com a gente</p>
+      <div>
+        <div>
+          <h4>Nossas informações</h4>
+          <p>avenida paulista 2000 - 5o andar</p>
+          <p>+55 (11) 00000-0000</p>
+          <p>contato@vezalige.com</p>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.2673031606296!2d-46.66106008551874!3d-23.558841384684126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59cd060ea13b%3A0x7c535e9d9784e6c2!2sAv.%20Paulista%2C%202000%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-200!5e0!3m2!1spt-BR!2sbr!4v1587078961081!5m2!1spt-BR!2sbr"
+            width="350"
+            height="300"
+            frameBorder="0"
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+          />
+        </div>
+        <div>
+          <h4>Envie uma mensagem</h4>
+          <input placeholder={"Nome*"} />
+          <input placeholder={"Email*"} />
+          <textarea placeholder={"Menssagem*"} />
+          <button>Enviar</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import Information from "./Information/Information";
+import CallToAction from "./CallToAction/CallToAction";
+import Contact from "./Contact/Contact";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Information />
+      <CallToAction />
+      <Contact />
     </>
   );
 };
